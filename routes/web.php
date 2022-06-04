f916dab9f8a306ad42b8245024de40dde9445c53
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-06-03 23:10:10 -0500

    Finalizando el registro de unusarios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
