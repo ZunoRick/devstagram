8c30b2a387f1d76f45ecdfa93acec988d3187c8b
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-06-26 16:01:53 -0500

    Agregando shells utiles

---
+++ refresh.sh
@@ -0,0 +1,3 @@
+./vendor/laravel/sail/bin/sail php artisan migrate:rollback
+./vendor/laravel/sail/bin/sail php artisan migrate
+./vendor/laravel/sail/bin/sail php artisan migrate:refresh --seed
---
+++ sailup.sh
@@ -0,0 +1,2 @@
+./vendor/laravel/sail/bin/sail up 
+./vendor/laravel/sail/bin/sail npm run watch-poll
