78e357e3062752c7ea3863c546485e31de9476a5
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-07-09 16:28:21 -0500

    Agregando componentes

---
+++ resources/views/components/listar-post.blade.php
@@ -0,0 +1,26 @@
+<div>
+	@if ($posts->count())
+		<div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+			@foreach ($posts as $post)
+				<div>
+					<a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
+						<img src="{{ asset('uploads') . '/' . $post->user->username . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+					</a>
+				</div>
+			@endforeach
+		</div>
+
+		<div class="my-10">
+			{{ $posts->links() }}
+		</div>
+	@else
+		<p class="text-gray-600 uppercase text-sm text-center font-bold">No hay Posts, sigue a alguien para poder mostrar sus posts</p>
+	@endif
+
+	{{-- Otra forma de recorrer el arreglo
+	@forelse ($post as $post)
+		
+	@empty
+		
+	@endforelse --}}
+</div>
